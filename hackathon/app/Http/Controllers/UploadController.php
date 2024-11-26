96f746fc6bd561afeb6197c54a19b7a41cf1085f
Ricardo <ricardoervilha456@gmail.com>
2024-11-26 16:28:03 -0300

    Troca de mensagens entre laravel e python

--- hackathon/app/Http/Controllers/UploadController.php
+++ hackathon/app/Http/Controllers/UploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class UploadController extends Controller
 {
@@ -12,7 +13,9 @@ class UploadController extends Controller
     public function receive_dataset(Request $request){
         if ($request->hasFile('file_input')) {
             $file = $request->file('file_input');
-            return json_encode($file->getClientOriginalName());
+            $url = "http://127.0.0.1:5000/name";
+            $response = Http::post($url, [json_encode($file->getClientOriginalName())]);
+            dd(json_decode($response));
         } else {
             return dd("Nenhum arquivo enviado.");
         }
