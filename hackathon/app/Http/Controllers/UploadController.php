5959a800aeb661b4cdbfc6842c619bec15f4756b
Ricardo <ricardoervilha456@gmail.com>
2024-11-26 17:54:49 -0300

    Lógica para armazenamento do CSV dentro do banco de dados

--- hackathon/app/Http/Controllers/UploadController.php
+++ hackathon/app/Http/Controllers/UploadController.php
@@ -12,9 +12,13 @@ class UploadController extends Controller
     }
     public function receive_dataset(Request $request){
         if ($request->hasFile('file_input')) {
-            $file = $request->file('file_input');
-            $url = "http://127.0.0.1:5000/name";
-            $response = Http::post($url, [json_encode($file->getClientOriginalName())]);
+            #-------------------------------------
+            $file = $request->file('file_input'); #recover the file
+            $url = "http://127.0.0.1:5000/save_csv"; #url to store the csv file in the database
+            $response = Http::attach(
+                'file_input', file_get_contents($file), $file->getClientOriginalName()
+            )->post($url); #response HTTP
+            #-------------------------------------    
             dd(json_decode($response));
         } else {
             return dd("Nenhum arquivo enviado.");
