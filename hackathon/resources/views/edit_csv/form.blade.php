21b934669149f4cbd68cdfa05b19b833362389a2
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2024-11-27 22:19:08 -0300

    estilizando o formulário de edição de tipos do csv

--- hackathon/resources/views/edit_csv/form.blade.php
+++ hackathon/resources/views/edit_csv/form.blade.php
@@ -1,23 +1,45 @@
-<form action="{{ route('csv.update') }}" method="POST">
-    @csrf
-    <input value="{{ $name }}" name="table_name" type="text" hidden>
-    @foreach ($collection as $key => $item)
-        <div class="">
-            <label>{{ $key }}</label>
-            <input name="{{ $key . "_type"}}" type="text" value="{{ $item }}">
-            <div class="flex items-center mb-4">
-                <input type="checkbox" name="{{ $key . "_checkbox" }}">
-                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Feature de Tempo</label>
+<div class="flex flex-col h-full items-center">
+    <h1 class="text-xl mb-4">Ajeitando os dados</h1>
+    <form action="{{ route('csv.update') }}" method="POST" 
+          class="flex flex-col overflow-y-auto h-full p-4 border border-b-[#D94929] rounded-lg">
+        @csrf
+        <input value="{{ $name }}" name="table_name" type="text" hidden>
+        @foreach ($collection as $key => $item)
+            <div class="flex flex-col border-b-2 border-b-[#D94929] mb-4">
+                <div class="flex justify-between items-center">
+                    <label class="text-gray-700">{{ $key }}</label>
+                    <input 
+                        name="{{ $key . '_type' }}" 
+                        type="text" 
+                        value="{{ $item }}" 
+                        class="border border-gray-300 rounded-lg p-2">
+                </div>
+                <div class="flex items-center mt-2">
+                    <input type="checkbox" name="{{ $key . '_checkbox' }}">
+                    <label for="default-checkbox" 
+                           class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        Feature de Tempo
+                    </label>
+                </div>
+                <div class="flex items-center mt-2">
+                    <input type="checkbox" name="{{ $key . '_americanformat' }}">
+                    <label for="default-checkbox" 
+                           class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        É formato americano ?
+                    </label>
+                </div>
+                <div class="flex justify-between items-center mt-4">
+                    <label>Digite o separador, caso seja uma data.</label>
+                    <input 
+                        name="{{ $key . '_separator' }}" 
+                        type="text" 
+                        class="border border-gray-300 rounded-lg p-2">
+                </div>
             </div>
-            <div class="flex items-center mb-4">
-                <input type="checkbox" name="{{ $key . "_americanformat" }}">
-                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">É formato americano ?</label>
-            </div>
-            <div>
-                <label>Digite o separador, caso seja uma data.</label>
-                <input name="{{ $key . "_separator"}}" type="text" place>
-            </div>
-        </div>
-    @endforeach
-    <button class="bg-blue-400" type="submit">Submeter as alterações</button>
-</form>
+        @endforeach
+        <button type="submit" 
+                class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Submeter alterações
+        </button>
+    </form>
+</div>
